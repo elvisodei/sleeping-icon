29d08960a8d60266f75902e23c50a9a0fba68b1e
elvisodei <elvisodei124@gmail.com>
2023-02-20 04:51:15 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Sad)
+    soundExpression.sad.playUntilDone()
+    basic.showIcon(IconNames.Asleep)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showIcon(IconNames.Happy)
+    soundExpression.giggle.playUntilDone()
+    basic.showIcon(IconNames.Asleep)
+})
+basic.showIcon(IconNames.Asleep)
